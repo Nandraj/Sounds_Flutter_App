3c51b597623508749d40d4a33ec67fdd52d7c065
Nandraj <nandraj.rathod.009@gmail.com>
2020-07-30 19:48:20 +0530

    Xylophone issue of missing asset solved with change in pubspec.yaml

--- lib/views/xylophone/xylophone_screen.dart
+++ lib/views/xylophone/xylophone_screen.dart
@@ -19,6 +19,7 @@ class XylophoneScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
